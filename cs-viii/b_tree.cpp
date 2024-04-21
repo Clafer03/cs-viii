f604287a904e0375e20e2b628cf5d6bfa03e65fc
Clafer03 <claudi125.lol@gmail.com>
2024-04-20 20:41:36 -0500

    Reorder BTree

--- cs-viii/b_tree.cpp
+++ cs-viii/b_tree.cpp
@@ -7,7 +7,8 @@ class BTreeNode{
         int t, n;
         bool leaf;
         vector<int> keys;
-        vector<BTreeNode*> children;     
+        vector<BTreeNode*> children;  //vector que contiene la clase BTreeNode, haciendo posible la generación de los nodos hijos
+
     public:
         BTreeNode(int _t, bool _leaf)
         {
@@ -38,32 +39,72 @@ class BTreeNode{
             if(leaf){
                 while(i >= 0 && keys[i] > k){
                     keys[i+1] = keys[i];
-                    i = i -1;
+                    i -= 1;
                 }
                 keys[i+1] = k;
-                n = n + 1;
+                n += 1;
+            }
+            else{
+                while(i >= 0 && keys[i] > k){
+                    i += 1;
+                }
+                if(children[i+1]->n==2*t+1){
+                    splitChild(i+1, children[i+1]);
+                    if(keys[i+1] < k){
+                        i += 1;
+                    }
+                }
+                children[i+1]->insertNonFull(k);
             }
 
         }
-        splitChild(int i, BTreeNode* y);
+        void splitChild(int i, BTreeNode* y){
+            BTreeNode* z = new BTreeNode(y->t, y->leaf);
+            z-> n = t-1;
+            for(int j = 0; j < t-1; j++){
+                z->keys[j] = y->keys[j+t];
+            }
+            if(!y->leaf){
+                for(int j = 0; j < t; j++){
+                    z->children[j] = y->children[j+t];
+                }
+            }
+            y->n = t - 1;
 
-
+            for(int j = n; j >= i + 1; j--){
+                children[j+1] = children[j];
+            }
+            children[i+1] = z;
+            for(int j = n-1; j>= i; j--){
+                keys[j+1] = keys[j];
+            } 
+            keys[i] = y->keys[t-1];
+            n += 1;
+        }
 };
 
 class BTree{
     private:
-        BTreeNode* raiz;
+        BTreeNode* root;
         int t;
     public:
         
         BTree(int _t){
-            raiz = nullptr;
+            root = nullptr;
             t = _t;
         }
 
         void traverse(){
-            if(raiz != nullptr){
-                traverse() = raiz;
+            if(root != nullptr){
+                root->traverse();
+            }
+        }
+
+        void insert(int k){
+            if(root = nullptr){
+                root = new BTreeNode(t, true);
+                root->keys[0] = keys;
+                root.n = 1;
             }
         }
 };
